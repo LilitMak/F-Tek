42a54d33b9d8529a4907b288821254a5ee3f5e4a
LilitMak <lilka.2076@mail.ru>
2018-11-26 16:48:43 +0300

    js

--- js/js.js
+++ js/js.js
@@ -10,6 +10,7 @@ $('.slider').slick({
 });
 var link = document.querySelector(".login-link");
 var popup = document.querySelector(".modal-login");
+var close1 = document.querySelector(".js-close-campaign");
 var close = document.querySelector(".js-close-thank-you");
 
 link.addEventListener("click", function(evt) {
@@ -17,7 +18,50 @@ link.addEventListener("click", function(evt) {
   popup.classList.add("modal-show");
 });
 
+close1.addEventListener("click", function(evt){
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
 close.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.remove("modal-show");
+});
+
+
+// Отправка заявки
+$(document).ready(function() {
+	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+		if (document.form.name.value == '' || document.form.phone.value == '' ) {
+			valid = false;
+			return valid;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$('.js-overlay-thank-you').fadeIn();
+			$(this).find('input').val('');
+			$('#form').trigger('reset');
+		});
+		return false;
+	});
+});
+
+// Закрыть попап «спасибо»
+$('.js-close-thank-you').click(function() { // по клику на крестик
+	$('.js-overlay-thank-you').fadeOut();
+});
+
+$(document).mouseup(function (e) { // по клику вне попапа
+    var popup = $('.popup');
+    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+        $('.js-overlay-thank-you').fadeOut();
+    }
+});
+
+// Маска ввода номера телефона (плагин maskedinput)
+$(function($){
+	$('[name="phone"]').mask("+7(999) 999-9999");
 });
